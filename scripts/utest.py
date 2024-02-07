d35e60f42c5c8a3951d6daf1ebd000c0206dddb1
krassowski <5832902+krassowski@users.noreply.github.com>
2024-02-07 13:46:43 +0000

    Move test configuration to `setup.cfg`, add omit

--- scripts/utest.py
+++ scripts/utest.py
@@ -1,38 +1,14 @@
-""" run python unit tests with pytest
-"""
-import platform
+""" run python unit tests with pytest"""
 import sys
 
 import pytest
 
-OS = platform.system()
-PY = "".join(map(str, sys.version_info[:2]))
-
-OS_PY_ARGS = {
-    # notebook and ipykernel releases do not yet support python 3.8 on windows
-    # ("Windows", "38"): ["-k", "not serverextension"]
-}
-
-DEFAULT_ARGS = ["--cov-fail-under=100"]
-
 
 def run_tests():
     """actually run the tests"""
-    sys.path.insert(0, "python_packages/jupyter_lsp/")
-    args = [
-        "--cov",
-        "jupyter_lsp",
-        "--cov-report",
-        "term-missing:skip-covered",
-        "--no-cov-on-fail",
-        "-p",
-        "no:warnings",
-        # "--flake8",
-        "-vv",
-        *OS_PY_ARGS.get((OS, PY), DEFAULT_ARGS),
-    ] + list(sys.argv[1:])
+    list(sys.argv[1:])
 
-    return pytest.main(args)
+    return pytest.main()
 
 
 if __name__ == "__main__":
